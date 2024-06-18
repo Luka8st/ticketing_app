813f25ed072b87394d5ef510cd38841d376d3157
Luka8st <luka.bursic5@gmail.com>
2024-06-18 11:51:23 +0200

    style changes

--- resources/views/agents/show-ticket.blade.php
+++ resources/views/agents/show-ticket.blade.php
@@ -1,6 +1,7 @@
 <x-agents.layout>
-    <div class="grid grid-cols-3 gap-2 border ring rounded p-3 relative z-10">
-        <div class="col-span-2 border-r-2 border-r-gray-400 px-4">
+    <div class="grid grid-cols-4 gap-2">
+        <div class="col-span-3 border-r-2 border-r-gray-400 px-4 ">
+            <div class="border ring rounded p-3 relative z-10">
             Title: {{ $ticket->title }}
             <br>
             Description: {{ $ticket->description }}
@@ -16,7 +17,31 @@
             @endif
             <br>
             Created at: {{ $ticket->created_at->format('H:i d.m.Y') }}
+            </div>
+            @if ($ticket->status == 'new')
+                <form method="POST" action="{{ route('agent.tickets.open', ['ticket' => $ticket->id]) }}" class="mt-4">
+                    @csrf
+                    @method('PATCH')
+                    <x-forms.button>Start Resolving</x-forms.button>
+                </form>
+            @elseif ($ticket->status == 'open')
+                <div class="grid grid-cols-2 divide-x-2 h-20">
+                    <div></div>
+                    <div class="px-2 text-gray-600 content-center h-20 relative z-0">Opened at:
+                        {{ $ticket->opened_at->format('H:i d.m.Y') }}</div>
+                </div>
 
+                <form method="POST" action="{{ route('agent.tickets.close', ['ticket' => $ticket->id]) }}">
+                    @csrf
+                    @method('PATCH')
+                    <div class="border ring rounded p-3 relative z-0">
+                        <x-forms.textarea label="Closing comment" name="closing comment" class="w-full" />
+                    </div>
+                    <div class="mt-4">
+                        <x-forms.button>Mark as closed</x-forms.button>
+                    </div>
+                </form>
+            @endif
 
         </div>
         <div class="px-4">
@@ -27,28 +52,6 @@
         </div>
 
     </div>
-    @if ($ticket->status == 'new')
-        <form method="POST" action="{{ route('agent.tickets.open', ['ticket' => $ticket->id]) }}" class="mt-4">
-            @csrf
-            @method('PATCH')
-            <x-forms.button>Start Resolving</x-forms.button>
-        </form>
-    @elseif ($ticket->status == 'open')
-        <div class="grid grid-cols-2 divide-x-2 h-20">
-            <div></div>
-            <div class="px-2 text-gray-600 content-center h-20 relative z-0">Opened at: {{ $ticket->opened_at->format('H:i d.m.Y') }}</div>
-        </div>
-        
-        <form method="POST" action="{{ route('agent.tickets.close', ['ticket' => $ticket->id]) }}">
-            @csrf
-            @method('PATCH')
-            <div class="border ring rounded p-3 relative z-0">
-                <x-forms.textarea label="Closing comment" name="closing comment" class="w-full"/>
-            </div>
-            <div class="mt-4">
-                <x-forms.button>Mark as closed</x-forms.button>
-            </div>
-        </form>
-    @endif
+
 
 </x-agents.layout>
