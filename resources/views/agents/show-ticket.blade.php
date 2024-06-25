35eda8ca393d361c086a0997260c9d7adacfe8ce
Luka8st <luka.bursic5@gmail.com>
2024-06-25 11:51:13 +0200

    style

--- resources/views/agents/show-ticket.blade.php
+++ resources/views/agents/show-ticket.blade.php
@@ -1,20 +1,22 @@
 <x-agents.layout>
     <div class="grid grid-cols-4 gap-2">
         <div class="col-span-3 border-r-2 border-r-gray-400 px-4 ">
-            <div class="border ring rounded p-3 relative z-10">
-                Title: {{ $ticket->title }}
-                <br>
-                Description: {{ $ticket->description }}
-                <br>
+            <div class="border ring rounded relative z-10">
+                <h1 class="font-bold text-lg m-3"> {{ $ticket->title }} </h1>
+                <hr class="border-t-2 border-gray-500/50">
+                <div class="bg-gray-200 p-3">
+                    {{ $ticket->description }}
+                    <br>
 
-                @if ($ticket->images)
-                    @foreach ($ticket->images as $image)
-                        <button onclick="window.open('{{ asset($image->path) }}', '_blank')"><img
-                                src="{{ asset($image->path) }}" class="w-20 h-20" /></button>
-                    @endforeach
-                @endif
-                <br>
-                Created at: {{ $ticket->created_at->format('H:i d.m.Y') }}
+                    @if ($ticket->images)
+                        @foreach ($ticket->images as $image)
+                            <button onclick="window.open('{{ asset($image->path) }}', '_blank')"><img
+                                    src="{{ asset($image->path) }}" class="w-20 h-20" /></button>
+                        @endforeach
+                    @endif
+                    <br>
+                    <div class="text-sm italic">Created at: {{ $ticket->created_at->format('H:i d.m.Y') }}</div>
+                </div>
             </div>
             @if (Auth::user()->department == $ticket->department)
                 @if ($ticket->status == 'new')
@@ -26,7 +28,6 @@
                     </form>
                 @elseif ($ticket->status == 'open')
                     <div class="grid grid-cols-2 divide-x-2 h-20">
-                        <div></div>
                         <div class="px-2 text-gray-600 content-center h-20 relative z-0">Opened at:
                             {{ $ticket->opened_at->format('H:i d.m.Y') }}</div>
                     </div>
--- resources/views/tickets/show-ticket.blade.php
+++ resources/views/tickets/show-ticket.blade.php
@@ -1,29 +1,32 @@
 <x-layout>
     <div class="grid grid-cols-4 gap-2 relative z-0">
         <div class="col-span-3 border-r-2 border-r-gray-400 px-4">
-            <div class="rounded p-3 relative z-10 bg-white/10 ring mt-4">
-                <b class="text-blue-500/70">Title</b>: {{ $ticket->title }}
-                <br>
-                <b class="text-blue-500/70">Description</b>: {{ $ticket->description }}
-                <br>
-                @if ($ticket->files)
-                    @foreach ($ticket->files as $file)
-                        <button onclick="window.open('{{ asset($file) }}', '_blank')"><img src="{{ asset($file) }}"
-                                class="w-20 h-20" /></button>
-                    @endforeach
-                @endif
-                <br>
-                <div class="text-sm italic">Created at: {{ $ticket->created_at->format('H:i d.m.Y') }}</div>
+            <div class="rounded relative z-10 bg-white/10 ring">
+                <h1 class="font-bold text-lg p-3"> {{ $ticket->title }} </h1>
+                <hr class="border-t-2 border-gray-500/50">
+                <div class="m-3">
+                    {{ $ticket->description }}
+                    <br>
+                    @if ($ticket->files)
+                        @foreach ($ticket->files as $file)
+                            <button onclick="window.open('{{ asset($file) }}', '_blank')"><img src="{{ asset($file) }}"
+                                    class="w-20 h-20" /></button>
+                        @endforeach
+                    @endif
+                    <div class="text-sm italic mt-8">Created at: {{ $ticket->created_at->format('H:i d.m.Y') }}</div>
+                </div>
             </div>
             <hr class="my-8">
             <div class="bg-white/10 p-3 rounded mb-4">
-                Agent <i>{{ $ticket->agent->name }}</i> opened this ticket at {{ $ticket->opened_at->format('H:i d.m.Y') }}
+                Agent <i>{{ $ticket->agent->name }}</i> opened this ticket at
+                {{ $ticket->opened_at->format('H:i d.m.Y') }}
             </div>
 
             @if ($ticket->status == 'closed')
                 <hr class="my-8">
                 <div class="bg-white/10 p-3 rounded mb-4">
-                    Agent <i>{{ $ticket->agent->name }}</i> closed this ticket at {{ $ticket->closed_at->format('H:i d.m.Y') }} with a closing message: 
+                    Agent <i>{{ $ticket->agent->name }}</i> closed this ticket at
+                    {{ $ticket->closed_at->format('H:i d.m.Y') }} with a closing message:
                     <textarea class="w-full px-2 rounded h-10 resize-none" disabled>{{ $ticket->closing_comment }}</textarea>
                 </div>
             @endif
